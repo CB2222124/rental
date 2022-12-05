6cbae804297aaef5966c8378293d32fd313ce380
CB2222124 <2222124@student.uwtsd.ac.uk>
2022-12-05 21:18:05 +0000

    Added docker configurations to spin up postgres container for development

--- src/main/java/com/github/cb2222124/rental/Main.java
+++ src/main/java/com/github/cb2222124/rental/Main.java
@@ -1,7 +1,18 @@
 package com.github.cb2222124.rental;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/rental";
+
+    public static void main(String[] args) throws SQLException {
+        Properties properties = new Properties();
+        properties.setProperty("user", "postgres");
+        properties.setProperty("password", "postgres");
+        Connection connection = DriverManager.getConnection(URL, properties);
     }
 }
