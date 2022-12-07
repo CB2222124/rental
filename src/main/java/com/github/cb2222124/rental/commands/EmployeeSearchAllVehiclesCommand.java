e1946521669758df03e2d04d02f07db31299c8ae
Lez-Dev <eldev.kelly@gmail.com>
2022-12-07 10:05:08 +0000

    added sql tables

---
+++ src/main/java/com/github/cb2222124/rental/commands/EmployeeSearchAllVehiclesCommand.java
@@ -0,0 +1,17 @@
+package com.github.cb2222124.rental.commands;
+
+import com.github.cb2222124.rental.Application;
+import com.github.cb2222124.rental.models.Command;
+import com.github.cb2222124.rental.models.Role;
+import com.github.cb2222124.rental.utils.Postgres;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class EmployeeSearchAllVehicles implements Command {
+
+
+}
