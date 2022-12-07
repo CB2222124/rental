e8ede44e30d044841e3ab1382d4ef60125d3fe66
CB2222124 <2222124@student.uwtsd.ac.uk>
2022-12-07 17:34:37 +0000

    Connected functionally, some refactoring

--- src/main/java/com/github/cb2222124/rental/commands/ViewVehiclesAsCustomer.java
+++ src/main/java/com/github/cb2222124/rental/commands/ViewVehiclesAsCustomer.java
@@ -9,110 +9,100 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Map;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /**
- * author @ Rania
+ * @author Rania
  */
 public class ViewVehiclesAsCustomer implements Command {
 
     Scanner scanner = new Scanner(System.in);
+
     @Override
-    public void execute() {
-
-        System.out.println("Enter location ID: \n");
-        int locationID = Integer.parseInt(scanner.nextLine());
-
+    public void execute(HashMap<String, String> args) {
         try {
             Postgres postgres = new Postgres();
-            searchVehicle(locationID,postgres.getConnection());
-
+            //Take city name from user and show matching addresses.
+            System.out.print("Enter city name: ");
+            String city = scanner.nextLine();
+            String cityCode = getCityCode(city, postgres.getConnection());
+            showAddresses(cityCode, postgres.getConnection());
+            //Take address ID from user and find matching location.
+            System.out.print("Enter address ID: ");
+            int addressID = scanner.nextInt();
+            int locationID = getLocationID(addressID, postgres.getConnection());
+            //Show available vehicles at the given location.
+            showAvailableVehicles(locationID, postgres.getConnection());
             postgres.getConnection().close();
         } catch (SQLException e) {
-            System.out.println("Connection failed.");
-            System.out.println(e);
+            System.out.println("Connection failed, search aborted.");
+            e.printStackTrace();
+        } catch (NoSuchElementException e) {
+            if (e.getMessage() == null) {
+                System.out.println("Invalid input, search aborted.");
+            } else {
+                System.out.println(e.getMessage() + ", search aborted.");
+            }
         }
     }
 
-
-    private boolean searchVehicle(int locationID, Connection connection) throws SQLException {
-        PreparedStatement statement = connection.prepareStatement(
-                "SELECT * FROM vehicle WHERE location_id = ? AND available = true");
-
-        statement.setInt(1, locationID);
-
-
+    public String getCityCode(String cityName, Connection connection) throws SQLException, NoSuchElementException {
+        PreparedStatement statement = connection.prepareStatement("SELECT city_code FROM city WHERE city = ? ");
+        statement.setString(1, cityName);
         ResultSet result = statement.executeQuery();
-        printResults(result,"vehicle_id","reg" ,"make","model" , "available", "location_id" , "daily_fee");
-        return result.next();
+        if (result.next()) return result.getString("city_code");
+        throw new NoSuchElementException("No address found for given city");
     }
 
-    public boolean getLocation(int addressID, Connection connection) throws SQLException {
-
-        PreparedStatement statement = connection.prepareStatement(
-                "SELECT location_id FROM location WHERE address_id = ? ");
-        statement.setInt(1, addressID);
-
-
-        ResultSet result = statement.executeQuery();
-
-        return result.next();
-    }
-    public int getAddress(String cityCode, Connection connection) throws SQLException {
-
-        PreparedStatement statement = connection.prepareStatement(
-                "SELECT * FROM address WHERE city_code = ? ");
+    public void showAddresses(String cityCode, Connection connection) throws SQLException, NoSuchElementException {
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM address WHERE city_code = ? ");
         statement.setString(1, cityCode);
-
-
         ResultSet result = statement.executeQuery();
-        printResults(result, "Address Number", "House Number", "Street", "Postcode");
-
-        System.out.println("Please select address number");
-        int addressNumber = Integer.parseInt(scanner.nextLine());
-        return addressNumber;
+        if (!result.isBeforeFirst()) throw new NoSuchElementException("No address found for given city");
+        printResults(result, "address_id", "num", "street", "city_code", "country_code", "postcode");
     }
 
-    public String getCityCode(String cityName, Connection connection)throws SQLException {
+    public int getLocationID(int addressID, Connection connection) throws SQLException, NoSuchElementException {
+        PreparedStatement statement = connection.prepareStatement("SELECT location_id FROM location WHERE address_id = ? ");
+        statement.setInt(1, addressID);
+        ResultSet result = statement.executeQuery();
+        if (result.next()) return result.getInt("location_id");
+        throw new NoSuchElementException("No location found for given address");
+    }
 
-
-            PreparedStatement statement = connection.prepareStatement(
-                    "SELECT city_code FROM city WHERE city = ? ");
-            statement.setString(1, cityName);
-
-            ResultSet result = statement.executeQuery();
-            while (result.next()) {
-                String cityCode = result.getString("city_code");
-                System.out.println(cityCode);
-                return cityCode;
-            }
-        return null;
+    private void showAvailableVehicles(int locationID, Connection connection) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM vehicle WHERE location_id = ? AND available = true");
+        statement.setInt(1, locationID);
+        ResultSet result = statement.executeQuery();
+        if (result.isBeforeFirst()) {
+            printResults(result, "vehicle_id", "reg", "make", "model", "available", "location_id", "daily_fee");
+        } else {
+            System.out.println("No vehicles found.");
+        }
     }
 
     public static void printResults(ResultSet data, String... columnNames) throws SQLException {
         for (String column : columnNames) {
             System.out.printf("%s\t", column);
         }
-        System.out.printf("\n");
-
+        System.out.print("\n");
         while (data.next()) {
             for (String column : columnNames) {
                 System.out.printf("%s\t", data.getString(column));
             }
-            System.out.printf("\n");
+            System.out.print("\n");
         }
     }
 
-
-
     @Override
     public boolean isAvailable() {
-        return Application.role == Role.NONE;
-        }
+        return Application.user.getRole() == Role.CUSTOMER;
+    }
 
     @Override
     public String getDescription() {
-        return "Vehicles available at specific location";
+        return "Vehicles available at specific location.";
     }
 }
