5030cfe8001e0abfaa1dde1c6d0db17782ed802d
CB2222124 <2222124@student.uwtsd.ac.uk>
2022-12-07 15:21:00 +0000

    Added JUnit test, fixed a parsing bug

---
+++ src/test/java/utils/CommandParserTests.java
@@ -0,0 +1,92 @@
+package utils;
+
+import static org.junit.Assert.assertEquals;
+
+import com.github.cb2222124.rental.utils.CommandParser;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class CommandParserTests {
+
+    @Test
+    public void empty() {
+        String string = "";
+        CommandParser parser = new CommandParser();
+        assertEquals("", parser.getCommand(string));
+    }
+
+    @Test
+    public void command() {
+        String string = "login";
+        CommandParser parser = new CommandParser();
+        assertEquals("login", parser.getCommand(string));
+    }
+
+    @Test
+    public void quotedCommand() {
+        String string = "\"login\"";
+        CommandParser parser = new CommandParser();
+        assertEquals("login", parser.getCommand(string));
+    }
+
+    @Test
+    public void contaminatedCommand() {
+        String string = "\"login";
+        CommandParser parser = new CommandParser();
+        assertEquals("login", parser.getCommand(string));
+    }
+
+    @Test
+    public void commandWithOtherTokens() {
+        String string = "login one \"two and three\"";
+        CommandParser parser = new CommandParser();
+        assertEquals("login", parser.getCommand(string));
+    }
+
+    @Test
+    public void commandWithArgumentsAndTokens() {
+        String string = "login -one value \"two and three\"";
+        CommandParser parser = new CommandParser();
+        assertEquals("login", parser.getCommand(string));
+    }
+
+    @Test
+    public void argument() {
+        String string = "login -one";
+        CommandParser parser = new CommandParser();
+        HashMap<String, String> expected = new HashMap<>();
+        expected.put("one", "");
+        assertEquals(expected, parser.getArguments(string));
+    }
+
+    @Test
+    public void argumentAndValue() {
+        String string = "login -one value";
+        CommandParser parser = new CommandParser();
+        HashMap<String, String> expected = new HashMap<>();
+        expected.put("one", "value");
+        assertEquals(expected, parser.getArguments(string));
+    }
+
+    @Test
+    public void argumentsAndValues() {
+        String string = "login -one value -two \"second value\"";
+        CommandParser parser = new CommandParser();
+        HashMap<String, String> expected = new HashMap<>();
+        expected.put("one", "value");
+        expected.put("two", "second value");
+        assertEquals(expected, parser.getArguments(string));
+    }
+
+    @Test
+    public void argumentsAndValuesAndTokens() {
+        String string = "login -one value -two \"second value\" random tokens -three value3";
+        CommandParser parser = new CommandParser();
+        HashMap<String, String> expected = new HashMap<>();
+        expected.put("one", "value");
+        expected.put("two", "second value");
+        expected.put("three", "value3");
+        assertEquals(expected, parser.getArguments(string));
+    }
+}
