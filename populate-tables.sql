21dfd4b5ed2f57057469c96927ee3a81b0ef1360
CB2222124 <2222124@student.uwtsd.ac.uk>
2022-12-07 10:35:52 +0000

    Update tables

--- populate-tables.sql
+++ populate-tables.sql
@@ -1,4 +1,6 @@
 -- https://www.mockaroo.com/
+insert into employee (username, password) values ('admin', 'password');
+
 insert into city (city_code, city) values ('SW', 'Swansea');
 insert into city (city_code, city) values ('BS', 'Bristol');
 insert into city (city_code, city) values ('GL', 'Glasgow');
@@ -16,20 +18,19 @@ insert into address (num, street, city_code, country_code, postcode) values (22,
 insert into address (num, street, city_code, country_code, postcode) values (123, 'Arthur Close', 'GL', 'SC', 'GL3 6HH');
 
 
-insert into location (address_id) values (1);
-insert into location (address_id) values (2);
-insert into location (address_id) values (3);
-
-
 insert into customer (firstname, lastname, username, password, address_id) values ('Jamila', 'Jones', 'user101', 'letmein', 4);
 insert into customer (firstname, lastname, username, password, address_id) values ('Barney', 'Rubble', 'BRubble101', 'sticksnstones', 5);
 
 
-insert into vehicle (reg_no, make, model, available, location_id, daily_fee) values ('GH26 VVC', 'Honda', 'Ridgeline', true, 1, 124.11);
-insert into vehicle (reg_no, make, model, available, location_id, daily_fee) values ('RR02 CSE', 'GMC', 'Acadia', true, 1, 99.53);
-insert into vehicle (reg_no, make, model, available, location_id, daily_fee) values ('HX51 OLP', 'Ford', 'Mustang', true, 1, 130.58);
-insert into vehicle (reg_no, make, model, available, location_id, daily_fee) values ('VC88 HDV', 'Bentley', 'Continental GT', true, 2, 139.72);
-insert into vehicle (reg_no, make, model, available, location_id, daily_fee) values ('HR91 JAO', 'Chevrolet', 'Silverado', true, 3, 186.17);
+insert into location (address_id) values (1);
+insert into location (address_id) values (2);
+insert into location (address_id) values (3);
+
+insert into vehicle (reg, make, model, available, location_id, daily_fee) values ('GH26 VVC', 'Honda', 'Ridgeline', true, 1, 124.11);
+insert into vehicle (reg, make, model, available, location_id, daily_fee) values ('RR02 CSE', 'GMC', 'Acadia', true, 1, 99.53);
+insert into vehicle (reg, make, model, available, location_id, daily_fee) values ('HX51 OLP', 'Ford', 'Mustang', true, 1, 130.58);
+insert into vehicle (reg, make, model, available, location_id, daily_fee) values ('VC88 HDV', 'Bentley', 'Continental GT', true, 2, 139.72);
+insert into vehicle (reg, make, model, available, location_id, daily_fee) values ('HR91 JAO', 'Chevrolet', 'Silverado', true, 3, 186.17);
 
 
-insert into booking (customer_id, pickup_loc, dropoff_loc, reg_no, datefrom, dateto) values (2, 3, 1, 'GH26 VVC', '2022-12-10', '2022-12-15');
+insert into booking (customer_id, vehicle_id, pickup_loc, dropoff_loc, datefrom, dateto) values (2, 3, 3, 1, '2022-12-10', '2022-12-15');
