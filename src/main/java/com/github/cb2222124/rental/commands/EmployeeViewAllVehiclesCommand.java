8001085a9e70b94275c7b30e2cce270fbf10dd10
CB2222124 <2222124@student.uwtsd.ac.uk>
2022-12-08 10:13:32 +0000

    Replace user input fetch with argument eval

--- src/main/java/com/github/cb2222124/rental/commands/EmployeeViewAllVehiclesCommand.java
+++ src/main/java/com/github/cb2222124/rental/commands/EmployeeViewAllVehiclesCommand.java
@@ -11,49 +11,30 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
 
 /**
- * @author: Liam
+ * @author Liam
  */
-
 public class EmployeeViewAllVehiclesCommand implements Command {
-
-    Scanner scanner = new Scanner(System.in);
 
     @Override
     public void execute(HashMap<String, String> args) {
         try {
             Postgres postgres = new Postgres();
-            System.out.print("To filter by booked vehicles enter 'booked', otherwise enter any key: ");
-            String filter = scanner.nextLine();
-            //indicate whether user wants to filter by booked
-            boolean userChoice = applyFilter(filter);
-            if(!userChoice) {
-                try {
-                    searchAllVehiclesUnfiltered(postgres.getConnection());
-                } catch (NoSuchElementException e){
-                        System.out.println("Error");
-                }
+            boolean bookedOnly = args.containsKey("booked");
+            if (bookedOnly) {
+                showBookedVehicles(postgres.getConnection());
+
             } else {
-                try {
-                    searchAllVehiclesFiltered(postgres.getConnection());
-                } catch (NoSuchElementException e){
-                        System.out.println("Error");
-                }
+                showAllVehicles(postgres.getConnection());
             }
             postgres.getConnection().close();
         } catch (SQLException e) {
-            System.out.print("Error connecting to database");
+            System.out.print("Error connecting to database, search aborted.");
         }
     }
 
-    public boolean applyFilter(String filter){
-        return filter.equalsIgnoreCase("booked");
-    }
-
-    public void searchAllVehiclesUnfiltered (Connection connection) throws SQLException {
+    public void showAllVehicles(Connection connection) throws SQLException {
         PreparedStatement statement = connection.prepareStatement("SELECT * FROM vehicle");
         ResultSet result = statement.executeQuery();
         if (result.isBeforeFirst()) {
@@ -65,12 +46,12 @@ public class EmployeeViewAllVehiclesCommand implements Command {
         }
     }
 
-    public void searchAllVehiclesFiltered (Connection connection) throws SQLException {
+    public void showBookedVehicles(Connection connection) throws SQLException {
         PreparedStatement statement = connection.prepareStatement("select vehicle.vehicle_id, vehicle.reg, vehicle.make, vehicle.model, booking.customer_id, booking.datefrom, booking.dateto FROM vehicle INNER JOIN booking ON booking.vehicle_id = vehicle.vehicle_id ORDER BY vehicle.vehicle_id;");
         ResultSet result = statement.executeQuery();
         if (result.isBeforeFirst()) {
             String[] resultColumns = {"vehicle_id", "reg", "make", "model", "customer_id", "datefrom", "dateto"};
-            String[] outputColumns = {"Vehicle ID", "Registration", "Make", "Model", "Customer ID", "Date booking starts", "Date booking ends"};
+            String[] outputColumns = {"Vehicle ID", "Registration", "Make", "Model", "Customer ID", "Collection Date", "Return Date"};
             new OutputFormatter().printResultSet(result, resultColumns, outputColumns);
         } else {
             System.out.println("No vehicles found.");
@@ -78,9 +59,12 @@ public class EmployeeViewAllVehiclesCommand implements Command {
     }
 
     @Override
-    public boolean isAvailable() { return Application.user.getRole() == Role.EMPLOYEE; }
+    public boolean isAvailable() {
+        return Application.user.getRole() == Role.EMPLOYEE;
+    }
 
     @Override
-    public String getDescription() { return "Search all available vehicles with an option to filter by booked"; }
-
+    public String getDescription() {
+        return "View all vehicles or filter by booked only by adding -booked.";
+    }
 }
