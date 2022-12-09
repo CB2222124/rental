57cafae1eee480791e90350fc22f1ce89ce3d3e2
CB2222124 <2222124@student.uwtsd.ac.uk>
2022-12-09 20:59:19 +0000

    Refactor and document customer view vehicles

--- src/main/java/com/github/cb2222124/rental/commands/vehicle/ViewVehiclesCommandCustomer.java
+++ src/main/java/com/github/cb2222124/rental/commands/vehicle/ViewVehiclesCommandCustomer.java
@@ -13,7 +13,13 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /**
- * @author Rania, Callan
+ * Customer command to view available vehicles at a specified location and filter by make or model.
+ * Optional arguments: -city Used to specify a city to limit search for locations.
+ * -city Used to specify a country to limit search for locations.
+ * -make Used to filter vehicles at the chosen location by make.
+ * -model Used to filter vehicles at the chosen location by model.
+ *
+ * @author Rania, Callan.
  */
 public class ViewVehiclesCommandCustomer implements Command {
 
@@ -21,8 +27,7 @@ public class ViewVehiclesCommandCustomer implements Command {
     @Override
     public void execute(LinkedHashMap<String, String> args) {
 
-        try {
-            Postgres postgres = new Postgres();
+        try (Postgres postgres = new Postgres()) {
             Scanner scanner = new Scanner(System.in);
 
             //Read optional arguments.
@@ -37,16 +42,12 @@ public class ViewVehiclesCommandCustomer implements Command {
             showLocations(cityCode, countryCode, postgres.getConnection());
 
             //Show vehicles that match users arguments.
-            //Note: Currently make model filter is applied once the customer commits to a location (Per the brief),
-            //a future iteration might allow the user to find a location that has a certain make.
+            //Note: Currently make model filter is applied once the customer commits to a location (Per the brief).
+            //A future iteration might allow the user to find a location that has a certain make.
             System.out.print("Enter Location ID to search: ");
             int locationID = scanner.nextInt();
             showAvailableVehicles(make, model, locationID, postgres.getConnection());
-
-            postgres.getConnection().close();
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
             System.out.println("Error connecting to database, search aborted.");
         } catch (NoSuchElementException e) {
             if (e.getMessage() == null) {
@@ -55,9 +56,17 @@ public class ViewVehiclesCommandCustomer implements Command {
                 System.out.println(e.getMessage() + ", search aborted.");
             }
         }
-
     }
 
+    /**
+     * Finds the city code associated with a specified city.
+     *
+     * @param cityName   City to search for.
+     * @param connection The Postgres connection to execute command on.
+     * @return The city code for given city.
+     * @throws SQLException           Database errors.
+     * @throws NoSuchElementException No city code found for the given city.
+     */
     public String getCityCode(String cityName, Connection connection) throws SQLException, NoSuchElementException {
         PreparedStatement statement = connection.prepareStatement("SELECT city_code FROM city WHERE city = ? ");
         statement.setString(1, cityName);
@@ -66,6 +75,15 @@ public class ViewVehiclesCommandCustomer implements Command {
         throw new NoSuchElementException("No addresses found for given city");
     }
 
+    /**
+     * Finds the country code associated with a specified city.
+     *
+     * @param countryName Country to search for.
+     * @param connection  The Postgres connection to execute command on.
+     * @return The country code for given country.
+     * @throws SQLException           Database errors.
+     * @throws NoSuchElementException No country code found for the given country.
+     */
     public String getCountryCode(String countryName, Connection connection) throws SQLException, NoSuchElementException {
         PreparedStatement statement = connection.prepareStatement("SELECT country_code FROM country WHERE country = ? ");
         statement.setString(1, countryName);
@@ -74,6 +92,13 @@ public class ViewVehiclesCommandCustomer implements Command {
         throw new NoSuchElementException("No addresses found for given country");
     }
 
+    /**
+     * Outputs a table constructed from a location, and it's associated address.
+     *
+     * @param connection The Postgres connection to execute command on.
+     * @throws SQLException           Database errors.
+     * @throws NoSuchElementException No locations were found.
+     */
     private void showLocations(String cityCode, String countryCode, Connection connection) throws SQLException {
         CallableStatement statement = connection.prepareCall("{call locationCityCountryCodeSearch(?, ?)}");
         statement.setString(1, cityCode);
@@ -88,6 +113,15 @@ public class ViewVehiclesCommandCustomer implements Command {
         }
     }
 
+    /**
+     * Outputs vehicles at a specific location that match the specified make and model (if provided).
+     *
+     * @param make       Make to filter by (Accepts empty string).
+     * @param model      Model to filter by (Accepts empty string).
+     * @param locationID The ID of the location to search.
+     * @param connection The Postgres connection to execute command on.
+     * @throws SQLException Database errors.
+     */
     private void showAvailableVehicles(String make, String model, int locationID, Connection connection) throws SQLException {
         CallableStatement statement = connection.prepareCall("{call customerLocMakeModelSearch(?, ?, ?)}");
         statement.setString(1, make);
